725bdf5ffa015e3e95ee8325a9680470ed71866c
mcedwards <morgan@taftfurniture.com>
2020-02-13 15:55:53 -0500

    Fixed build js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist',
-    filename: '[name].[chunkhash].js'
+    filename: 'build.js'
   },
   optimization: {
     splitChunks: {
